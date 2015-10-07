5bfb928f69e8edca57f39d4a11a3f579640dc666
slagogonzalez <slagogonzalez@atanasoff20.danielcastelao.org>
2015-10-07 11:05:06 +0200

    Programacion1

---
+++ src/boletin2/pkg1/Boletin21_B.java
@@ -0,0 +1,12 @@
+
+package boletin2.pkg1;
+public class Boletin21_B {
+
+ 
+    public static void main(String[] args) {
+        float base = 2.0f ;
+        float altura =(float)3.0 ;//cast
+       System.out.println("area ="+ base*altura/2) ;
+    }
+    
+}
